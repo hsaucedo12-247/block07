ef862b680ba1831a84bd71d7cdc04c44f0e3483e
Humberto Saucedo <hsaucedo6@gmail.com>
2024-08-31 21:26:51 -0600

    vault js

---
+++ script.js
@@ -0,0 +1,18 @@
+//create a string:you have received this message because you have been chosen to open an important vault. Here is the secret combination
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination")
+//assign 3 variables 10 40 39
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination-10 , 40 , 39.");
+//variable 1
+//5 + 5 gives us our first result
+let firstSum = 5 + 5;
+console.log(firstSum)
+//variable 2
+//first result * 4 = 40
+let firstproduct = firstSum * 4;
+console.log(firstproduct);
+//variable 3
+//second result - 1 = 39
+let x= firstproduct - 1;
+console.log(x)
+
+//add comments
